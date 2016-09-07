64b5f93635d2caecdadc7e438f35c36a51fa9a39
Sean Stahly <seanstahly@gmail.com>
2016-09-06 23:30:02 -0500

    added readme updated to include href url

---
+++ README.md
@@ -0,0 +1,12 @@
+This script will run scan through an html document and change all of the hrefs to a location of your choosing.
+
+
+run via 
+./replaceHref.sh 'path to email' 'new href email'
+
+for example:
+./replaceHref.sh testEmail.html http://www.google.com
+
+
+if the script does not work you may need to run 
+'chmod +x replaceHref.sh'
--- replaceHref.sh
+++ replaceHref.sh
@@ -2,9 +2,9 @@
 import os, shutil, re, sys
 
 #changedEmail = sys.argv[1]
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
 
-	if os.path.isfile('./htmlEmail'):
+	if os.path.isdir('./htmlEmail'):
 		shutil.rmtree('./htmlEmail')
 	os.mkdir('./htmlEmail', 0755)
 	newEmail = ""
@@ -16,11 +16,12 @@ if len(sys.argv) > 1:
 		for l in content:
 			line = l
 			if "href=\"" in line:
-				newEmail += regex.sub('href="http://www.google.com\"', line)
+				newEmail += regex.sub('href="' + sys.argv[2] + '\"', line)
+				#newEmail += regex.sub('href="http://www.google.com\"', line)
 				#print line.replace('href\"*\"', 'href\"http://www.google.com\"')
 			else:
 				newEmail += line
 	with open('./htmlEmail/newEmail.html', 'w+') as f:
 		f.write(newEmail)
 else:
-	print 'Please enter the location of the email'
+	print 'Please enter the location of the email and/or the new href'
