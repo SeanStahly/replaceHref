5a322dd16f311e217e7c8877181e4588453ed559
Sean Stahly <seanstahly@gmail.com>
2016-09-06 22:58:34 -0500

    First commit

---
+++ replaceHref.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os, shutil, re, sys
+
+#changedEmail = sys.argv[1]
+if len(sys.argv) > 1:
+
+	if os.path.isfile('./htmlEmail'):
+		shutil.rmtree('./htmlEmail')
+	os.mkdir('./htmlEmail', 0755)
+	newEmail = ""
+
+
+	with open('testEmail.txt') as f:
+		content = f.readlines()
+		regex = re.compile(r'href=\".*\"', re.IGNORECASE)
+		for l in content:
+			line = l
+			if "href=\"" in line:
+				newEmail += regex.sub('href="http://www.google.com\"', line)
+				#print line.replace('href\"*\"', 'href\"http://www.google.com\"')
+			else:
+				newEmail += line
+	with open('./htmlEmail/newEmail.html', 'w+') as f:
+		f.write(newEmail)
+else:
+	print 'Please enter the location of the email'
